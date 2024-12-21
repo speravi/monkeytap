b40a2a89af78a3b464bb2516d18cbf68bd8a7e5f
speravi <91125469+speravi@users.noreply.github.com>
2024-12-21 17:47:51 +0200

    save game config to localstorage

--- frontend/src/Pages/SettingsPage.tsx
+++ frontend/src/Pages/SettingsPage.tsx
@@ -5,8 +5,12 @@ import LayoutSelector from "../Components/Config/LayoutSelector";
 import RegisterGapClickToggle from "../Components/Config/RegisterGapClickToggle";
 import ThemeSelector from "../Components/Config/ThemeSelector";
 import TimerSelector from "../Components/Config/TimerSelector";
+import { useGame } from "../GameContext";
 
 const SettingsPage = () => {
+  //TODO: idk if i like this here
+  const { dispatch } = useGame();
+
   return (
     <div className="w-full max-w-4xl mx-auto">
       <h1 className="text-2xl mb-4">Game Settings</h1>
@@ -38,6 +42,15 @@ const SettingsPage = () => {
         <div className="mb-4">
           <h2 className="text-xl mb-2">Theme</h2>
           <ThemeSelector />
+        </div>
+        {/* TODO: Create a component? */}
+        <div className="mb-4">
+          <button
+            onClick={() => dispatch({ type: "RESET_TO_DEFAULT" })}
+            className="p-2 bg-red-500 text-white rounded"
+          >
+            Reset to Default
+          </button>
         </div>
       </div>
     </div>
