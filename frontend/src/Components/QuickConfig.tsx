c958f931911c0744d6a02a952d8436e44f165a3a
Deivismdev <deivismdev@gmail.com>
2025-04-13 17:53:35 +0300

    add a button to quickconfig to reset settings to default

--- frontend/src/Components/QuickConfig.tsx
+++ frontend/src/Components/QuickConfig.tsx
@@ -1,17 +1,34 @@
+import { IconRotate } from "@tabler/icons-react";
 import ActiveTileCountSelector from "./Config/ActiveTileCountSelector";
 import GridSizeSelector from "./Config/GridSizeSelector";
 import GridTileGapSelector from "./Config/GridTileGapSelector";
 import LayoutSelector from "./Config/LayoutSelector";
 import TimerSelector from "./Config/TimerSelector";
 import Spacer from "./Spacer";
+import { useState } from "react";
+import ConfirmActionModal from "./ConfirmActionModal";
+import { useGame } from "../GameContext";
 
 const QuickConfig = () => {
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const { dispatch } = useGame();
+  const handleResetSettingsConfirm = () => {
+    dispatch({ type: "RESET_TO_DEFAULT" });
+    setIsConfirmModalOpen(false);
+  };
   return (
     <div
       className={`
-          bg-elementBg rounded-md text-sm flex mb-2 p-2 flex-col
-        `}
+        relative bg-elementBg rounded-md text-sm flex mb-2 p-2 flex-col
+      `}
     >
+      <button
+        onClick={() => setIsConfirmModalOpen(true)}
+        className="absolute top-2 right-2 p-1 text-inactive hover:text-active cursor-pointer z-10"
+        title="Reset settings to default"
+      >
+        <IconRotate size={20} />
+      </button>
       <div className="lg:pb-5">
         <div className="flex justify-center">
           <LayoutSelector />
@@ -47,6 +64,13 @@ const QuickConfig = () => {
           </div>
         </div>
       </div>
+      <ConfirmActionModal
+        isOpen={isConfirmModalOpen}
+        onClose={() => setIsConfirmModalOpen(false)}
+        onConfirm={handleResetSettingsConfirm}
+        title="Reset Settings"
+        message="Are you sure you want to reset settings to their default values?"
+      />
     </div>
   );
 };
